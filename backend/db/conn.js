030cc84a3dc60fb6c2690925031d5fcb0fcd3c62
Gabriel Galvan <98289206+GalvanGabe@users.noreply.github.com>
2024-03-15 21:31:50 -0300

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1/getapet')
-    console.log('Conectou ao Mongoose!')
 }
 
 main().catch((err) => console.log(err))
